bc7b8cfa14a85386e55bffb5cb4745046048c9a2
Daniel Chukwu <00danzy@gmail.com>
2022-07-30 16:29:51 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main (String [] args){
+
+        // create instance of class UnitConverter
+        UnitConverter converter = new UnitConverter();
+        converter.home();
+
+    }
+}
